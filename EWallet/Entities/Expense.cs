d0855e8f79638c7fc43c643028b742d85891b8bc
Abbass Tabikh <abbasstabikh87774@gmail.com>
2023-11-07 15:51:28 +0200

    spacing

--- EWallet/Entities/Expense.cs
+++ EWallet/Entities/Expense.cs
@@ -5,7 +5,6 @@
         public byte Category { get; set; }
         public string Name { get; set; } = string.Empty;
         public decimal Price { get; set; }
-
         public Budget Budget { get; set; }
         public Guid BudgetID { get; set; }
     }
